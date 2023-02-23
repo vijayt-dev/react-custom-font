6dbba4d3e9b8cc795257a55cf997c9f5c98673e7
vijayt-dev <vijayatk3@gmail.com>
2023-02-23 15:37:17 +0530

    Added Custom Font Configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App container">
+      <div className="flex">
+        <div className="card">
+          <div className="card-header">Default Font</div>
+          <div className="card-body">
+            <h5 className="card-title">Card Title</h5>
+            <p className="card-text">
+              With supporting text below as a natural lead-in to additional
+              content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+        <div className="card custom-font">
+          <div className="card-header">Custom Font</div>
+          <div className="card-body">
+            <h5 className="card-title">Card Title</h5>
+            <p className="card-text">
+              With supporting text below as a natural lead-in to additional
+              content.
+            </p>
+            <a href="#" className="btn btn-primary">
+              Go somewhere
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
